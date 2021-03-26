29f3b22212419a8d2bea3ce43290328dcbe8237f
Kevin Li <kevinli020508@gmail.com>
2021-03-26 13:49:11 +0800

    Rename exported files to end with .list

--- tocfl_inkstone_converter.py
+++ tocfl_inkstone_converter.py
@@ -18,7 +18,7 @@ with open("tocfl.csv", "r") as csvfile:
     print(f"Read {str(csvreader.line_num)} rows")
     for i, rows in enumerate(levels, 1):
         print(f"    - {len(rows)} in level {str(i)}")
-        with open(f"tocfl_level_{i}.csv", mode="w") as export_file:
+        with open(f"tocfl_level_{i}.list", mode="w") as export_file:
             writer = csv.DictWriter(export_file, fieldnames=export_fields, delimiter="\t")
             for row in rows:
                 writer.writerow(row)
